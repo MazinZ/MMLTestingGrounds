336d10bb011cf06ca192907b1fb7744a8bbde6cd
Greezus <mattg13@vt.edu>
2015-01-01 23:21:31 -0500

    Level 3 Done

--- Ants/Ants/ViewController.swift
+++ Ants/Ants/ViewController.swift
@@ -20,7 +20,9 @@ func buttonClicker(inout myButton: UIButton!, inout myButtonNum: Int, maxNum: In
     }
 }
 
-
+func initializeButton(inout myButton: UIButton!) -> Void {
+    myButton.setTitle("1", forState: UIControlState.Normal)
+}
 
 class ViewController: UIViewController {
 
@@ -190,9 +192,61 @@ class SquareViewController: UIViewController {
     @IBOutlet weak var rightSumLab: UILabel!
     @IBOutlet weak var botSumLab: UILabel!
     
+    @IBOutlet weak var contButton: UIButton!
+    
+    
+    func calculateSquareSum(){
+        var winner = true
+        var topSumN: Int?
+        var leftSumN: Int?
+        var rightSumN: Int?
+        var botSumN: Int?
+        
+        
+        topSumN = topLeftButtonNum + topButtonNum + topRightButtonNum
+        rightSumN = topRightButtonNum + midRightButtonNum + botRightButtonNum
+        botSumN = botRightButtonNum + botMidButtonNum + botLeftButtonNum
+        leftSumN = topLeftButtonNum + midLeftButtonNum + botLeftButtonNum
+        
+        topSumLab.text = "\(topSumN!)"
+        rightSumLab.text = "\(rightSumN!)"
+        botSumLab.text = "\(botSumN!)"
+        leftSumLab.text = "\(leftSumN!)"
+        
+        if(rightSumN == 12 && botSumN == 12 && leftSumN == 12 && topSumN == 12){
+            var numberArray: [Int] = [ topLeftButtonNum, topButtonNum, topRightButtonNum, midLeftButtonNum, midRightButtonNum, botLeftButtonNum, botMidButtonNum, botRightButtonNum]
+            
+            for var i = 0 ; i<numberArray.count ; i++ {
+                for var j = 0 ; j<numberArray.count ; j++ {
+                    if(numberArray[i] == numberArray[j]){
+                        if(i != j){
+                            winner = false
+                        }
+                    }
+                }
+            }
+            if(winner){
+                contButton.hidden = false
+                println("Winner")
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initializeButton(&topLeftBut)
+        initializeButton(&topBut)
+        initializeButton(&topRightBut)
+        initializeButton(&midLeftBut)
+        initializeButton(&midRightBut)
+        initializeButton(&botLeftBut)
+        initializeButton(&botMidBut)
+        initializeButton(&botRightBut)
+        contButton.hidden = true
+        calculateSquareSum()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -202,26 +256,172 @@ class SquareViewController: UIViewController {
     
     @IBAction func topLeftClick() {
         buttonClicker(&topLeftBut, &topLeftButtonNum, 9)
+        calculateSquareSum()
     }
     @IBAction func topClick() {
         buttonClicker(&topBut, &topButtonNum, 9)
+        calculateSquareSum()
     }
     @IBAction func topRightClick() {
         buttonClicker(&topRightBut, &topRightButtonNum, 9)
+        calculateSquareSum()
     }
     @IBAction func midLeftClick() {
         buttonClicker(&midLeftBut, &midLeftButtonNum, 9)
+        calculateSquareSum()
     }
     @IBAction func midRightClick() {
         buttonClicker(&midRightBut, &midRightButtonNum, 9)
+        calculateSquareSum()
     }
     @IBAction func botLeftClick() {
         buttonClicker(&botLeftBut, &botLeftButtonNum, 9)
+        calculateSquareSum()
     }
     @IBAction func botMidClick() {
         buttonClicker(&botMidBut, &botMidButtonNum, 9)
+        calculateSquareSum()
     }
     @IBAction func botRightClick() {
         buttonClicker(&botRightBut, &botRightButtonNum, 9)
+        calculateSquareSum()
+    }
+}
+
+class PentagonViewController: UIViewController {
+    
+    @IBOutlet weak var aBut: UIButton!
+    @IBOutlet weak var bBut: UIButton!
+    @IBOutlet weak var cBut: UIButton!
+    @IBOutlet weak var dBut: UIButton!
+    @IBOutlet weak var eBut: UIButton!
+    @IBOutlet weak var fBut: UIButton!
+    @IBOutlet weak var gBot: UIButton!
+    @IBOutlet weak var hBot: UIButton!
+    @IBOutlet weak var iBot: UIButton!
+    @IBOutlet weak var jbot: UIButton!
+    
+    @IBOutlet weak var topLeftSumBut: UILabel!
+    @IBOutlet weak var topRightSumBut: UILabel!
+    @IBOutlet weak var botLeftSumBut: UILabel!
+    @IBOutlet weak var botSumBut: UILabel!
+    @IBOutlet weak var botRightSumBut: UILabel!
+    
+    var aButtonNum: Int = 1
+    var bButtonNum: Int = 1
+    var cButtonNum: Int = 1
+    var dButtonNum: Int = 1
+    var eButtonNum: Int = 1
+    var fButtonNum: Int = 1
+    var gButtonNum: Int = 1
+    var hButtonNum: Int = 1
+    var iButtonNum: Int = 1
+    var jButtonNum: Int = 1
+    
+    @IBOutlet weak var contbutton: UIButton!
+    
+    func calculatePentSum(){
+        var winner = true
+        var topLeftSumN: Int?
+        var topRightSumN: Int?
+        var botLeftSumN: Int?
+        var botRightSumN: Int?
+        var botSumN: Int?
+        
+        
+        topLeftSumN = aButtonNum + iButtonNum + jButtonNum
+        topRightSumN = aButtonNum + bButtonNum + cButtonNum
+        botLeftSumN = iButtonNum + hButtonNum + gButtonNum
+        botRightSumN = cButtonNum + dButtonNum + eButtonNum
+        botSumN = gButtonNum + fButtonNum + eButtonNum
+        
+        topLeftSumBut.text = "\(topLeftSumN!)"
+        topRightSumBut.text = "\(topRightSumN!)"
+        botLeftSumBut.text = "\(botLeftSumN!)"
+        botRightSumBut.text = "\(botRightSumN!)"
+        botSumBut.text = "\(botSumN!)"
+        
+        if(topLeftSumN == 14 && topRightSumN == 14 && botLeftSumN == 14 && botRightSumN == 14 && botSumN == 14){
+            var numberArray: [Int] = [ aButtonNum, bButtonNum, cButtonNum, dButtonNum, eButtonNum, fButtonNum, gButtonNum, hButtonNum, iButtonNum, jButtonNum]
+            
+            for var i = 0 ; i<numberArray.count ; i++ {
+                for var j = 0 ; j<numberArray.count ; j++ {
+                    if(numberArray[i] == numberArray[j]){
+                        if(i != j){
+                            winner = false
+                        }
+                    }
+                }
+            }
+            if(winner){
+                contbutton.hidden = false
+                println("Winner")
+            }
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        initializeButton(&aBut)
+        initializeButton(&bBut)
+        initializeButton(&cBut)
+        initializeButton(&dBut)
+        initializeButton(&eBut)
+        initializeButton(&fBut)
+        initializeButton(&gBot)
+        initializeButton(&hBot)
+        initializeButton(&iBot)
+        initializeButton(&jbot)
+        contbutton.hidden = true
+        calculatePentSum()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func aClick() {
+        buttonClicker(&aBut, &aButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func bClick() {
+        buttonClicker(&bBut, &bButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func cClick() {
+        buttonClicker(&cBut, &cButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func dClick() {
+        buttonClicker(&dBut, &dButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func eClick() {
+        buttonClicker(&eBut, &eButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func fClick() {
+        buttonClicker(&fBut, &fButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func gClick() {
+        buttonClicker(&gBot, &gButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func hClick() {
+        buttonClicker(&hBot, &hButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func iClick() {
+        buttonClicker(&iBot, &iButtonNum, 11)
+        calculatePentSum()
+    }
+    @IBAction func jClick() {
+        buttonClicker(&jbot, &jButtonNum, 11)
+        calculatePentSum()
     }
 }
